98111313e6d2da57de8435024c9a39dc10e31b41
Muhammad Owais <rahneverd@gmail.com>
2024-12-24 11:18:08 +0500

    user.module done

--- osshcloud-be/src/user/user.module.ts
+++ osshcloud-be/src/user/user.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserHistory } from './entities/user-history.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, UserHistory]),
+    JwtModule.register({
+      secret: process.env.JWT_CONSTANT,
+      signOptions: { expiresIn: process.env.JWT_EXPIRE_IN },
+    }),
+    // UserLogModule,
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
