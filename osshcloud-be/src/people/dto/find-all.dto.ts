f3cfc2ecb25a221d61bae93a99a043bd43822efc
Muhammad Owais <rahneverd@gmail.com>
2025-01-14 14:37:56 +0500

    people findall dto added

---
+++ osshcloud-be/src/people/dto/find-all.dto.ts
@@ -0,0 +1,53 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { paginationDto } from 'src/utils/commonDtos.dto';
+
+export class FindAllDataPayloadDto {
+  @IsOptional()
+  @IsNumber()
+  peopleId: number;
+
+  @IsOptional()
+  @IsString()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  middleName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage: string;
+
+  @IsOptional()
+  @IsString()
+  dateOfBirth: string;
+
+  @IsOptional()
+  @IsNumber()
+  lovGenderTypeId: number;
+}
+
+export class FindAllDto {
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => FindAllDataPayloadDto)
+  @IsArray()
+  data: FindAllDataPayloadDto[];
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => paginationDto)
+  pagination: paginationDto;
+}
