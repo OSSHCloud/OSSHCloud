360252d98812a4879fb123684ab12820e7db1968
Muhammad Owais <rahneverd@gmail.com>
2025-01-21 14:44:09 +0500

    people module updated

--- osshcloud-be/src/people/people.module.ts
+++ osshcloud-be/src/people/people.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { PeopleController } from './people.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { People } from './entities/people.entity';
+import { PeopleHistory } from './entities/people-history.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([People, PeopleHistory])],
   controllers: [PeopleController],
   providers: [PeopleService],
 })
-export class PeopleModule {}
+export class peopleModule {}
