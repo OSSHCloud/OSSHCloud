3bbea4ec209c17972088429b34eb58ac4dc3e04e
Muhammad Owais <rahneverd@gmail.com>
2025-01-23 14:46:23 +0500

    account entity added

--- osshcloud-be/src/account/entities/account.entity.ts
+++ osshcloud-be/src/account/entities/account.entity.ts
@@ -1,1 +1,62 @@
-export class Account {}
+import { Bank } from 'src/bank/entities/bank.entity';
+import { ListOfValues } from 'src/list-of-values/entities/list-of-values.entity';
+import { Person } from 'src/people/entities/person.entity';
+import { User } from 'src/user/entities/user.entity';
+import { LID_CREATED_ID } from 'src/utils/constants';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity({ name: 'account' })
+export class Account {
+  @PrimaryGeneratedColumn({ name: 'account_id' })
+  accountId: number;
+
+  @Column({ name: 'title' })
+  title: string;
+
+  @Column({ name: 'description', nullable: true })
+  description: string;
+
+  @ManyToOne(() => Person, (x) => x.personId)
+  @JoinColumn({ name: 'person_id' })
+  @Column({
+    name: 'person_id',
+  })
+  personId: number;
+
+  @ManyToOne(() => Bank, (x) => x.bankId)
+  @JoinColumn({ name: 'bank_id' })
+  @Column({
+    name: 'bank_id',
+  })
+  bankId: number;
+
+  @Column({ name: 'iban', nullable: true })
+  iban: string;
+
+  // @Column({ name: 'description', nullable: true })
+  // description: string;
+
+  // dml
+
+  @ManyToOne(() => ListOfValues, (x) => x.listOfValuesId)
+  @JoinColumn({ name: 'dml_status' })
+  @Column({
+    name: 'dml_status',
+    default: LID_CREATED_ID,
+  })
+  dmlStatus: number;
+
+  @ManyToOne(() => User, (user) => user.userId)
+  @JoinColumn({ name: 'dml_user_id' })
+  @Column({ name: 'dml_user_id' })
+  dmlUserId: number;
+
+  @Column({ name: 'dml_timestamp', type: 'timestamptz' })
+  dmlTimestamp: Date;
+}
