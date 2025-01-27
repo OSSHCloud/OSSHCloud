4283820881cbb0b5bbb7d2770f7b30cb130924ee
Muhammad Owais <rahneverd@gmail.com>
2025-01-27 14:48:44 +0500

    account service added

--- osshcloud-be/src/account/account.service.ts
+++ osshcloud-be/src/account/account.service.ts
@@ -1,26 +1,173 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository } from 'typeorm';
+import { Account } from './entities/account.entity';
+import { AccountHistory } from './entities/account-history.entity';
+import { CreateDataPayloadDto } from './dto/create.dto';
+import {
+  LID_DELETE_ID,
+  RECORD_EXISTS,
+  TRY_AGAIN_LATER,
+} from 'src/utils/constants';
+import { RestResponse } from 'src/utils/restResponse';
+import { isNumber, isEmpty } from 'lodash';
+import { FindAllDataPayloadDto } from './dto/find-all.dto';
+import { paginationDto } from 'src/utils/commonDtos.dto';
 
 @Injectable()
 export class AccountService {
-  create(createAccountDto: CreateAccountDto) {
-    return 'This action adds a new account';
+  constructor(
+    @InjectRepository(Account)
+    private readonly mainRepository: Repository<Account>,
+    @InjectRepository(AccountHistory)
+    private readonly historyRepositry: Repository<AccountHistory>,
+  ) {}
+
+  async create(params: CreateDataPayloadDto) {
+    // check if record exists
+    const ifRecordExists = await this.mainRepository.findOne({
+      where: [
+        {
+          iban: params.iban,
+          dmlStatus: Not(LID_DELETE_ID),
+        },
+      ],
+    });
+    if (ifRecordExists) {
+      return RestResponse.notFound(params, RECORD_EXISTS);
+    }
+    // create a new record
+    const res = await this.mainRepository.save({ ...params });
+    // create history record
+    await this.historyRepositry.save({ ...res });
+    if (res) {
+      return [res];
+    } else {
+      // If creation fails, throw a BadRequestException.
+      return RestResponse.error(params, TRY_AGAIN_LATER);
+    }
   }
 
-  findAll() {
-    return `This action returns all account`;
+  async findAll(params: FindAllDataPayloadDto, pagination: paginationDto) {
+    let sql = '';
+    // Construct SQL query based on provided filter parameters.
+    if (isNumber(params?.accountId)) {
+      sql += `r.accountId=${params?.accountId} AND `;
+    }
+    if (!isEmpty(params?.title)) {
+      sql += `r.title ilike '%${params?.title}%' AND `;
+    }
+    if (!isEmpty(params?.description)) {
+      sql += `r.description ilike '%${params?.description}%' AND `;
+    }
+    if (!isEmpty(params?.personId)) {
+      sql += `r.personId=${params?.personId} AND `;
+    }
+    if (!isEmpty(params?.bankId)) {
+      sql += `r.bankId=${params?.bankId} AND `;
+    }
+    if (!isEmpty(params?.iban)) {
+      sql += `r.iban=${params?.iban} AND `;
+    }
+
+    sql += `r.dmlStatus != ${LID_DELETE_ID} ORDER BY 1 DESC`;
+
+    // Count the total number of records based on the constructed SQL query.
+    const count = await this.mainRepository
+      .createQueryBuilder('r')
+      .where(sql)
+      .getCount();
+    // Apply pagination if provided and return the filtered and paginated records.
+    if (
+      !isEmpty(pagination) &&
+      pagination?.pageNo >= 0 &&
+      pagination?.itemsPerPage > 0
+    ) {
+      sql += ` OFFSET ${
+        pagination?.pageNo * pagination?.itemsPerPage
+      } ROWS FETCH NEXT ${pagination?.itemsPerPage} ROWS ONLY`;
+    }
+
+    const query = count
+      ? await this.mainRepository
+          .createQueryBuilder('r')
+          .where(sql)
+          .leftJoinAndSelect('r.bankId', 'bankId')
+          .leftJoinAndSelect('r.personId', 'personId')
+          .getMany()
+      : [];
+    return count ? [query, count] : [];
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} account`;
-  }
+  // async findOne(params: FindOneDataPayloadDto) {
+  //   const res = await this.mainRepository.findOne({
+  //     where: {
+  //       listOfValuesId: params?.listOfValuesId,
+  //       dmlStatus: Not(LID_DELETE_ID),
+  //     },
+  //   });
 
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
-  }
+  //   return res;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
-  }
+  // async update(params: UpdateDataPayloadDto) {
+  //   // check if record exists
+  //   const ifRecordExists = await this.mainRepository.findOne({
+  //     where: [
+  //       {
+  //         title: params.title,
+  //         dmlStatus: Not(LID_DELETE_ID),
+  //         lovCategoryId: params.lovCategoryId,
+  //         listOfValuesId: Not(params.listOfValuesId),
+  //       },
+  //     ],
+  //   });
+
+  //   if (ifRecordExists) {
+  //     return RestResponse.error(params, RECORD_EXISTS);
+  //   }
+
+  //   const obj = await this.findOne({
+  //     listOfValuesId: params.listOfValuesId,
+  //   });
+
+  //   // If the record to update does not exist, throw a NotFoundException.
+  //   if (!obj) {
+  //     return RestResponse.notFound(params);
+  //   }
+
+  //   const res = await this.mainRepository.save({
+  //     ...params,
+  //   });
+  //   await this.historyRepositry.save({ ...res });
+  //   if (!res) {
+  //     return [res];
+  //   } else {
+  //     // If creation fails, throw a BadRequestException.
+  //     return RestResponse.error(params, TRY_AGAIN_LATER);
+  //   }
+  // }
+
+  // async delete(params: DeleteDataPayloadDto) {
+  //   const obj = await this.findOne({
+  //     listOfValuesId: params.listOfValuesId,
+  //   });
+
+  //   // If the record to update does not exist, throw a NotFoundException.
+  //   if (!obj) {
+  //     return RestResponse.notFound(params);
+  //   }
+  //   obj.dmlStatus = params['dmlStatus'];
+  //   obj.dmlTimestamp = params['dmlTimestamp'];
+  //   const res = await this.mainRepository.save({
+  //     ...obj,
+  //   });
+  //   await this.historyRepositry.save({ ...res });
+  //   if (res) {
+  //     return [res];
+  //   } else {
+  //     // If creation fails, throw a BadRequestException.
+  //     return RestResponse.error(params, TRY_AGAIN_LATER);
+  //   }
+  // }
 }
