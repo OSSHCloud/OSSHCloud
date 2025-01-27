8f856a54845c8f7bd15bf7e3698f8837289c098b
Muhammad Owais <rahneverd@gmail.com>
2025-01-27 14:50:08 +0500

    account dtos added

---
+++ osshcloud-be/src/account/dto/find-all.dto.ts
@@ -0,0 +1,49 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { paginationDto } from 'src/utils/commonDtos.dto';
+
+export class FindAllDataPayloadDto {
+  @IsOptional()
+  @IsNumber()
+  accountId: number;
+
+  @IsOptional()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @IsOptional()
+  @IsNumber()
+  personId: number;
+
+  @IsOptional()
+  @IsNumber()
+  bankId: number;
+
+  @IsOptional()
+  @IsString()
+  iban: string;
+}
+
+export class FindAllDto {
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => FindAllDataPayloadDto)
+  @IsArray()
+  data: FindAllDataPayloadDto[];
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => paginationDto)
+  pagination: paginationDto;
+}
