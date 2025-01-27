806e9b61dd55c7a4f71d8a22b853ee8b969eebc7
Muhammad Owais <rahneverd@gmail.com>
2025-01-27 15:56:38 +0500

    email entity added

--- osshcloud-be/src/email/entities/email.entity.ts
+++ osshcloud-be/src/email/entities/email.entity.ts
@@ -1,1 +1,50 @@
-export class Email {}
+import { ListOfValues } from 'src/list-of-values/entities/list-of-values.entity';
+import { User } from 'src/user/entities/user.entity';
+import { LID_ACTIVE_ID, LID_CREATED_ID } from 'src/utils/constants';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity({ name: 'email' })
+export class Email {
+  @PrimaryGeneratedColumn({ name: 'email_id' })
+  emailId: number;
+
+  @ManyToOne(() => User, (x) => x.userId)
+  @JoinColumn({ name: 'user_id' })
+  @Column({ name: 'user_id' })
+  userId: number;
+
+  @Column({ name: 'email' })
+  email: string;
+
+  @ManyToOne(() => ListOfValues, (x) => x.listOfValuesId)
+  @JoinColumn({ name: 'lov_status_id' })
+  @Column({
+    name: 'lov_status_id',
+    default: LID_ACTIVE_ID,
+  })
+  lovStatusId: number;
+
+  // dml
+
+  @ManyToOne(() => ListOfValues, (x) => x.listOfValuesId)
+  @JoinColumn({ name: 'dml_status' })
+  @Column({
+    name: 'dml_status',
+    default: LID_CREATED_ID,
+  })
+  dmlStatus: number;
+
+  @ManyToOne(() => User, (x) => x.userId)
+  @JoinColumn({ name: 'dml_user_id' })
+  @Column({ name: 'dml_user_id', nullable: true })
+  dmlUserId: number;
+
+  @Column({ name: 'dml_timestamp', type: 'timestamptz' })
+  dmlTimestamp: Date;
+}
